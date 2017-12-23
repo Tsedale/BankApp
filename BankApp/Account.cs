4a84aaceb07e7a4370af96b3cd3c14b503dec4da
Tsedale Tadesse <tsedaletad@gmail.com>
2017-12-23 11:31:39 -0800

    Static

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -10,11 +10,28 @@ namespace BankApp
     class Account
     {
         #region Properties
-        public int AccountNumber { get; set; }
+        public int AccountNumber { get; private set; }
         public string EmailAddress { get; set; }
         public string AccountName { get; set; }
         public string AccountType { get; set; }
-        public decimal Balance { get; set; }
+        public decimal Balance { get; private set; }
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+            
         #endregion
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,6 +10,14 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
+            //Reference, Object, instance of Class
+            var account = new Account();
+            account.EmailAddress = "test@test.com";
+            account.AccountType = "Checking";
+
+            account.Deposit(100.10M);
+            Console.WriteLine($"{account.Balance }");
+        
         }
     }
 }
